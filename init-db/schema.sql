aa66965e9481ef77baf80f83a1b09c9d599b07a7
Thanon <fourdat.444@gmail.com>
2026-01-28 09:55:24 +0700

    add insert

--- init-db/schema.sql
+++ init-db/schema.sql
@@ -9,3 +9,4 @@ CREATE TABLE IF NOT EXISTS products (
 INSERT INTO products (name, price) VALUES ('bomb', 20);
 INSERT INTO products (name, price) VALUES ('car', 100);
 INSERT INTO products (name, price) VALUES ('tank', 250);
+INSERT INTO products (name, price) VALUES ('banana', 900);
